af874e5f8db8e27c5fb7be7c25e7a71a1e28239b
Adam Malantonio <adam.malantonio@gmail.com>
2015-07-15 13:59:07 -0400

    add minify step

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp')
+var path = require('path')
+
 var less = require('gulp-less')
 var rename = require('gulp-rename')
-var path = require('path')
+var minifyCss = require('gulp-minify-css')
 
 var lessOpts = {
   paths: [path.join(__dirname, 'less', 'includes')]
@@ -14,9 +16,16 @@ gulp.task('lessify', function () {
       .pipe(gulp.dest('./css'))
 })
 
-gulp.task('watch', function () {
-  gulp.watch('./less/**/*.less', ['lessify'])
+gulp.task('minify', function () {
+  gulp.src('./css/beetbox.css')
+      .pipe(minifyCss())
+      .pipe(rename(function(f) {f.basename += '.min'}))
+      .pipe(gulp.dest('./css'))
 })
 
-gulp.task('build', ['watch'])
+gulp.task('watch', function () {
+  gulp.watch('./less/**/*.less', ['build'])
+})
+
+gulp.task('build', ['lessify', 'minify'])
 gulp.task('default', ['build'])
